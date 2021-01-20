88711cb45b34d6f609b70254fb1b61a6af4976cb
Haresh Hanat <haresh.hanat@outlook.com>
2021-01-20 20:58:55 +0530

    vue demo Duplicate identifier 'export='.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,28 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+     <chat-window :current-user-id="currentUserId" :rooms="rooms" :messages="messages" />
+     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import ChatWindow from 'vue-advanced-chat';
+import 'vue-advanced-chat/dist/vue-advanced-chat.css';
 
 export default Vue.extend({
   name: 'Home',
   components: {
     HelloWorld,
+    ChatWindow,
+  },
+  data() {
+    return {
+      rooms: [],
+      messages: [],
+      currentUserId: 1234,
+    };
   },
 });
 </script>
